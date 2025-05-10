e34136e3e0c02d050ed66b123523c56d0ce74d7d
mhoss32@safeway.com <105231774+anwaralbertsons@users.noreply.github.com>
2025-05-09 22:29:13 -0500

    Jenkins successfully setup!.

--- app/src/main/java/me/dotcode/androidgit/MainActivity.kt
+++ app/src/main/java/me/dotcode/androidgit/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AndroidGitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Jenkins successfully working!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
